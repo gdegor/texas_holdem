55d5f6bed881d41dc837be97774000b28f70c42c
egovoryn <oleinick.egor@yandex.ru>
2023-04-19 00:29:02 +0700

    refactor

--- src/PokerHand.java
+++ src/PokerHand.java
@@ -1,27 +1,22 @@
 import java.util.*;
 
 public class PokerHand implements Comparable<PokerHand> {
-//    private final static ArrayList<Character> NOMINALS =
-//            new ArrayList<>(List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'));
     public static final ArrayList<Character> SUITS = new ArrayList<>(List.of('S', 'H', 'K', 'A'));
 //    S(pades), H(earts), D(iamonds), C(lubs)
 //    пики      сердца    ромбик      трефы
 
-    private ArrayList<Integer> nominals = new ArrayList<>();
-    private ArrayList<Character> suits = new ArrayList<>();
+    private final ArrayList<Integer> nominals = new ArrayList<>();
+    private final ArrayList<Character> suits = new ArrayList<>();
 
-
-    private final List<String> cardsInHand;
-    private int rank;   //  0..9
+    private final int rank;   //  0..9
 
     public PokerHand(String hand) {
-        cardsInHand = List.of(hand.split(" "));
-        separateByNominalsAndSuits();
-
+        List<String> cardsInHand = List.of(hand.split(" "));
+        separateByNominalsAndSuits(cardsInHand);
         rank = rankDetection();
     }
 
-    private void separateByNominalsAndSuits() {
+    private void separateByNominalsAndSuits(List<String> cardsInHand) {
         for (String card : cardsInHand) {
             char tmpChar = card.charAt(0);
             int tmpInt = Character.getNumericValue(tmpChar);
@@ -38,7 +33,6 @@ public class PokerHand implements Comparable<PokerHand> {
             }
             suits.add(card.charAt(1));
         }
-
         nominals.sort(Comparator.comparingInt(o -> o));
     }
 
@@ -52,14 +46,11 @@ public class PokerHand implements Comparable<PokerHand> {
     }
 
     private boolean isStraight() {
-        for (int x = 0; x < nominals.size() - 1; x++) {
-            if (nominals.get(x + 1) - nominals.get(x) != 1) {
-                return false;
-            }
+        for (int i = 0; i < nominals.size() - 1; i++) {
+            if (nominals.get(i + 1) - nominals.get(i) != 1) return false;
         }
         return true;
     }
-
 
     private int rankDetection() {
         HashMap<Integer, Integer> countNominals = countingIdentical(nominals);
@@ -69,30 +60,29 @@ public class PokerHand implements Comparable<PokerHand> {
         boolean isFlush = countSuits.get(suits.get(0)) == 5;
 
         if (isFlush) {
-            int countForFlushRoyal = 0;
             for (int i = Nominals.TEN.getIndex(); i <= Nominals.ACE.getIndex(); i++) {
-                if (nominals.contains(i)) countForFlushRoyal++;
+                if (!nominals.contains(i)) break;
+                if (i == Nominals.ACE.getIndex()) return Combinations.ROYAL_FLUSH.ordinal();
             }
-            if (countForFlushRoyal == 5) return Combinations.ROYAL_FLUSH.ordinal();
-            else if (isStraight) return Combinations.STRAIGHT_FLUSH.ordinal();
+            if (isStraight) return Combinations.STRAIGHT_FLUSH.ordinal();
         }
 
         int countPair = 0;
-        boolean three = false;
+        boolean hasThree = false;
         for (Map.Entry<Integer, Integer> entry : countNominals.entrySet()) {
             switch (entry.getValue()) {
                 case 5, 4 -> {
                     return Combinations.FOUR_OF_KIND.ordinal();
                 }
-                case 3 -> three = true;
+                case 3 -> hasThree = true;
                 case 2 -> countPair++;
             }
-            if (countPair == 1 && three) return Combinations.FULL_HOUSE.ordinal();
+            if (countPair == 1 && hasThree) return Combinations.FULL_HOUSE.ordinal();
         }
 
         if (isFlush) return Combinations.FLUSH.ordinal();
         else if (isStraight) return Combinations.STRAIGHT.ordinal();
-        else if (three) return Combinations.SET.ordinal();
+        else if (hasThree) return Combinations.SET.ordinal();
         else if (countPair == 2) return Combinations.TWO_PAIRS.ordinal();
         else if (countPair == 1) return Combinations.PAIR.ordinal();
 
@@ -102,10 +92,10 @@ public class PokerHand implements Comparable<PokerHand> {
     @Override
     public String toString() {
         return "\nPokerHand{" +
-                "cardsInHand=" + cardsInHand +
-//                ", rank=" + rank +
+                "nominals=" + nominals +
+                ", suits=" + suits +
                 ", combo=" + Combinations.values()[rank] +
-                "}";
+                '}';
     }
 
     @Override
